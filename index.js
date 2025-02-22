42bb75c5a7dca0faeb93ec5d1264c4c760145fb5
unknown <alazim-star>
2025-02-22 15:24:00 +0600

    add read me

--- index.js
+++ index.js
@@ -162,7 +162,7 @@ async function run() {
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 
