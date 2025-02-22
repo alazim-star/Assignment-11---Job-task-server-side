90af3ce98f8040671951603abe46e26536a9d5a7
unknown <alazim-star>
2025-02-22 16:47:31 +0600

    ws uninstall

--- index.js
+++ index.js
@@ -157,9 +157,9 @@ async function run() {
 
 
     //Confirm Connection
-    await client.db('admin').command({ ping: 1 });
+    // await client.db('admin').command({ ping: 1 });
  // Send a ping to confirm a successful connection
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
